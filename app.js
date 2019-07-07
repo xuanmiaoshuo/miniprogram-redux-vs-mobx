97faf18d5900f050ef7c31882d0b71db26e16a84
xuanmiaoshuo <yangshuo@meicai.cn>
2019-07-08 00:19:45 +0800

    添加redux

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-App({
-  onLaunch: function () {},
-})
+import { Provider } from 'wechat-weapp-redux'
+import { createStore } from 'redux'
+// ①action types
+const COUNTER_ADD = 'counter_add'
+const COUNTER_DEC = 'counter_dec'
+const COUNTER_DOUBLE = 'counter_double'
+
+// ②reducers
+const initialState = { a: 0 }
+
+function reducers(state = initialState, action) {
+  switch (action.type) {
+    case COUNTER_ADD:
+      return { ...state, a: state.a + 1 }
+    case COUNTER_DEC:
+      return { ...state, a: state.a - 1 }
+    case COUNTER_DOUBLE:
+      return { ...state, a: state.a * 2 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducers)
+
+// 向业务组件注入 store，actions，和 Provider 配合使用
+App(Provider(store)({
+  onLaunch: function () {}
+}))
--- pages/redux/redux.js
+++ pages/redux/redux.js
@@ -1,54 +1,27 @@
-import { observer } from 'wechat-weapp-mobx/observer'
-import { observable, action, decorate } from 'wechat-weapp-mobx/mobx'
+import { connect } from 'wechat-weapp-redux'
+// ①action types
+const COUNTER_ADD = 'counter_add'
+const COUNTER_DEC = 'counter_dec'
+const COUNTER_DOUBLE = 'counter_double'
 
-// 定义数据结构
-class Store {
-  a = 0
-}
+// ③action creator
+const incA = () => ({ type: COUNTER_ADD })
+const decA = () => ({ type: COUNTER_DEC })
+const double = () => ({ type: COUNTER_DOUBLE })
 
-// 定义对数据的操作
-class Action {
-  constructor({ store }) {
-    this.store = store
-  }
+const pageConfig = {}
 
-  incA = () => {
-    this.store.a++
-  }
-  decA = () => {
-    this.store.a--
-  }
-  double = () => {
-    this.store.a *= 2
-  }
-}
+// ④将state映射到组件data
+const mapStateToData = state => ({ store: state })
 
-// ① 使用 observable
-decorate(Store, {
-  a: observable,
+// ⑤将actions映射到组件的data
+const mapDispatchToPage = dispatch => ({
+  incA: () => dispatch(incA()),
+  decA: () => dispatch(decA()),
+  double: () => dispatch(double())
 })
 
-// ② 使用 action
-decorate(Action, {
-  incA: action,
-  decA: action,
-  double: action
-})
+// ⑥connect产生容器组件
+const nextPageConfig = connect(mapStateToData, mapDispatchToPage)(pageConfig)
 
-// ③实例化单一数据源
-const store = new Store()
-
-// ④实例化 actions，并且和 store 进行关联
-const actions = new Action({ store })
-
-// ⑤ 使用observer
-Page(observer({
-  // 向业务组件注入 store，actions，和 Provider 配合使用
-  props: {
-    store
-  },
-
-  incA: actions.incA,
-  decA: actions.decA,
-  double: actions.double
-}))
+Page(nextPageConfig)
